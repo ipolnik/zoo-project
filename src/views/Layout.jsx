6c5ce30ff31a0b56d35803b63977dab9ed42367a
Igor Polnik <ipolnik@yahoo.com>
2022-08-16 14:33:48 +0600

    layout

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,19 +1,20 @@
 const React = require('react');
 
-function Layout(props) {
+module.exports = function Layout({ children, username }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Zoo</title>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossOrigin="anonymous" />
+        <title>Hello, world!</title>
       </head>
       <body>
-        {props.children}
+        {children}
       </body>
+
+      <footer />
     </html>
   );
-}
-
-module.exports = Layout;
+};
