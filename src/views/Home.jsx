3b0bdd1c4f4de37e9cad6c8949b0291a9781ae09
Igor Polnik <ipolnik@yahoo.com>
2022-08-17 21:32:19 +0600

    map-update

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -34,7 +34,7 @@ function Home({ admin }) {
       </div>
       <h4 class="mainpage-title">Где мы находимся</h4>
       <div className="map">
-      <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aefc6000c688117eeba73795f065ca6dc2b17b48a567cd94dfba65d85b225ff86&amp;width=621&amp;height=470&amp;lang=ru_RU&amp;scroll=true"></script>
+      <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aefc6000c688117eeba73795f065ca6dc2b17b48a567cd94dfba65d85b225ff86&amp;width=1160&amp;height=470&amp;lang=ru_RU&amp;scroll=true"></script>
       </div>
       </div>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossOrigin="anonymous" />
