44a7f115724f71c12e8ccde024ade116c7935982
Edouard <professionnel.baillot@gmail.com>
2022-08-18 16:59:13 +0200

    routelist

--- src/routes/listeRoute.js
+++ src/routes/listeRoute.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const multer = require('multer');
 
+const { checkAdmin } = require('../middlewares/checkAdmin');
+
 const upload = multer({ dest: 'public/img/' });
 const {
   renderList, deleteAnimal, addAnimal, postAddAnimal,
@@ -8,10 +10,10 @@ const {
 
 router.get('/list', renderList);
 
-router.delete('/list/:id', deleteAnimal);
+router.delete('/list/:id', checkAdmin, deleteAnimal);
 
 router.route('/add')
-  .get(addAnimal)
+  .get(checkAdmin, addAnimal)
   .post(upload.array('photo', 10), postAddAnimal);
 
 module.exports = router;
